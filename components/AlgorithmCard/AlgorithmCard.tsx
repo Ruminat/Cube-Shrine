e0112b8f8c0ec96995a784f014a9e5469d078b58
Ruminat <Blowlyge@gmail.com>
2026-03-31 19:35:11 +0300

    Getting somewhere

--- components/AlgorithmCard/AlgorithmCard.tsx
+++ components/AlgorithmCard/AlgorithmCard.tsx
@@ -1,6 +1,7 @@
 import { MiniCube } from "@/components/MiniCube/MiniCube";
 import { AlgorithmNotation } from "@/components/AlgorithmNotation/AlgorithmNotation";
 import { Badge } from "@/components/UI/Badge/Badge";
+import { parseNotation } from "@/lib/notation/parser";
 import type { Algorithm } from "@/types/algorithm";
 import styles from "./AlgorithmCard.module.scss";
 
@@ -10,12 +11,14 @@ interface AlgorithmCardProps {
 }
 
 export function AlgorithmCard({ algorithm, onClick }: AlgorithmCardProps) {
+  const notationRotations = parseNotation(algorithm.notation);
+
   return (
     <article className={styles.card} onClick={() => onClick(algorithm)}>
       <h3 className={styles.title}>{algorithm.name}</h3>
       <AlgorithmNotation notation={algorithm.notation} />
       <div className={styles.cubeWrapper}>
-        <MiniCube preparationRotations={algorithm.preparationRotations} />
+        <MiniCube preparationRotations={notationRotations} />
       </div>
       <div className={styles.category}>
         <Badge>{algorithm.category}</Badge>
--- components/AlgorithmModal/AlgorithmModal.tsx
+++ components/AlgorithmModal/AlgorithmModal.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { useEffect } from "react";
 import { CubeRenderer } from "@/components/CubeRenderer/CubeRenderer";
 import { Button } from "@/components/UI/Button/Button";
+import { parseNotation } from "@/lib/notation/parser";
 import type { Algorithm } from "@/types/algorithm";
 import styles from "./AlgorithmModal.module.scss";
 
@@ -12,6 +14,16 @@ interface AlgorithmModalProps {
 
 export function AlgorithmModal({ algorithm, onClose }: AlgorithmModalProps) {
   if (!algorithm) return null;
+  const notationRotations = parseNotation(algorithm.notation);
+
+  useEffect(() => {
+    const onKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Escape") onClose();
+    };
+
+    window.addEventListener("keydown", onKeyDown);
+    return () => window.removeEventListener("keydown", onKeyDown);
+  }, [onClose]);
 
   return (
     <div className={styles.backdrop} onClick={onClose}>
@@ -21,7 +33,7 @@ export function AlgorithmModal({ algorithm, onClose }: AlgorithmModalProps) {
           <Button onClick={onClose}>Close</Button>
         </header>
         <p className={styles.notation}>{algorithm.notation}</p>
-        <CubeRenderer size={500} preparationRotations={algorithm.preparationRotations} />
+        <CubeRenderer size={500} preparationRotations={notationRotations} />
         <p className={styles.description}>{algorithm.description}</p>
       </div>
     </div>
